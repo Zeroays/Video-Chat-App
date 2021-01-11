b22e923cc68abee9fcd8f6d7642b5715339b9c5a
Vasu Rabaib <vrabaib@gmail.com>
2021-01-11 11:39:48 -0800

    Added separate username and room validation Functions; link to room appears only if public; clicking on link brings user to respective room (atm static); user can create new room with choice of public/private

--- server.js
+++ server.js
@@ -8,26 +8,55 @@ app.use(express.urlencoded({ extended: true }));
 
 const PORT = process.env.PORT || 3000;
 
-//Putting in sample data for rooms, to display
-//on client side
+//Format of rooms object -> subject to change
+
+// rooms ->
+//      roomName ->
+//                  users
+//                  type -> public/private
 const rooms = {
-  coders4life: {},
-  siliconValley: {},
+  coders4life: { users: {}, type: "public" },
+  siliconValley: { users: {}, type: "private" },
 };
 
 const usernames = [];
 
-app.post("/lobby", (req, res) => {
-  if (usernames.includes(req.body.username)) {
-    res.redirect("/");
-  } else {
-    usernames.push(req.body.username);
-    res.render("lobby", { rooms });
-  }
-});
+const validUsername = (usernames, name) => {
+  return !userNameExists(usernames, name) && !isNameEmpty(name);
+};
+
+const isNameEmpty = (name) => {
+  return name === "";
+};
+
+const userNameExists = (usernames, name) => {
+  return usernames.includes(name);
+};
+
+const roomExists = (rooms, roomName) => {
+  return roomName in rooms;
+};
 
 app.get("/", (req, res) => {
   res.render("enter");
+});
+
+app.get("/:room", (req, res) => {
+  if (!roomExists(rooms, req.params.room)) return res.redirect("/");
+  res.render("room", { roomName: req.params.room });
+});
+
+app.post("/lobby", (req, res) => {
+  console.log(req.body.username);
+  if (!validUsername(usernames, req.body.username)) return res.redirect("/");
+  usernames.push(req.body.username);
+  res.render("lobby", { rooms });
+});
+
+app.post("/room", (req, res) => {
+  if (roomExists(rooms, req.body.room)) return res.redirect("/");
+  rooms[req.body.room] = { users: {}, type: req.body.roomType };
+  res.redirect(req.body.room);
 });
 
 app.listen(PORT, () => {
